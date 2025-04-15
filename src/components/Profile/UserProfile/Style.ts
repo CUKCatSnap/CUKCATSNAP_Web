05d6281cef78f3edb93822c66b9cfdc5efd30564
khgeung <khgeung5357@naver.com>
2025-04-15 16:53:27 +0900

    feat : 유저의 마이페이지 구현

---
+++ src/components/Profile/UserProfile/Style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import {View, Pressable, Text} from 'react-native';
+
+export const Container = styled(View)`
+  margin: 5%;
+`;
---
+++ src/components/Profile/UserProfile/UserProfileComponent.tsx
@@ -0,0 +1,33 @@
+//유저로 로그인시 유저의 마이페이지를 보여주는 컴포넌트
+import React, {useState, useEffect, useCallback} from 'react';
+import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import * as S from './Style';
+import {useNavigation, useFocusEffect} from '@react-navigation/native';
+import {useSelector} from 'react-redux';
+import {fetchReviewAll} from '../../../apis/UserReserve/getReviewAll';
+import ReviewTag from '../../Review/ReviewTag';
+import LogoutBtn from '../../Logout/LogoutBtn';
+
+const UserProfileComponent = () => {
+  const navigation = useNavigation(); // 네비게이션 객체 가져오기
+
+  const isAuthor = useSelector(state => state.auth.user?.isAuthor);
+  const user = useSelector(state => state.auth.user);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <S.Container>
+        <ReviewTag />
+      </S.Container>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+});
+
+export default UserProfileComponent;
