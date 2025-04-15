eb724ea0d6ef4bcb7305e767ceef2b738599f017
khgeung <khgeung5357@naver.com>
2025-04-15 16:54:52 +0900

    feat : 유저의 리뷰를 전부 가져오는 api 추가 및 컴포넌트 생성

---
+++ src/apis/UserReserve/getReviewAll.ts
@@ -0,0 +1,50 @@
+//유저의 예약 목록을 불러오는 api입니다.(get)
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import apiClient from '../getAccessToken';
+
+export const fetchReviewAll = async (type = 'All', page, size) => {
+  try {
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    if (!accessToken) {
+      console.error('액세스 토큰이 없습니다.');
+      return null;
+    }
+
+    // 요청 파라미터 설정
+    const params = {
+      type: type, // 'UPCOMING' 또는 'ALL' 선택
+      pageable: {
+        page: page,
+        size: size,
+        sort: 'string', //이걸 파라미터로 같이 보내면 오류가 생김
+      },
+    };
+
+    // axios 요청 보내기
+    const response = await apiClient.get(
+      'https://api.catsnap.net/review/my/all',
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+
+        params: {
+          type: params.type,
+          page: params.pageable.page,
+          size: params.pageable.size,
+        },
+      },
+    );
+
+    console.log('응답 데이터:', response.data); // 응답 데이터 확인
+    return response.data;
+  } catch (error) {
+    console.error('요청 실패:', error.response?.data || error.message);
+    if (error.response) {
+      console.log('응답 상태:', error.response.status);
+      console.log('응답 데이터:', error.response.data);
+    }
+    return null;
+  }
+};
---
+++ src/components/Review/Style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import {View, Pressable, Text} from 'react-native';
+// isActive props 타입 정의
+interface TabProps {
+  isActive: boolean;
+}
+
+export const SearchTabContainer = styled(View)`
+  flex-direction: row;
+  justify-content: center;
+  gap: 10px;
+  margin: 5% 1%;
+`;
+
+export const SearchTab = styled(Pressable)`
+  align-items: center;
+  width: 50%;
+`;
+
+export const SearchTabText = styled(Text)`
+  color: #bebebe;
+  font-size: 14px;
+  margin-bottom: 5px;
+`;
+
+export const SearchTabBox = styled(View)<TabProps>`
+  width: 100%;
+  height: 3px;
+  background-color: ${props => (props.isActive ? '#423CD2' : '#BEBEBE')};
+`;
+
+export const SearchListContainerReview = styled(View)`
+  margin: 0%;
+`;
+export const SearchListContainerFeed = styled(View)`
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  width: 100%;
+  gap: 50%;
+`;
