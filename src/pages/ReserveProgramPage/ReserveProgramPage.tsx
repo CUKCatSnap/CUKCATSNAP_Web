88df33916d4a9be516b8c70d95394851d492d11d
khgeung <khgeung5357@naver.com>
2025-07-06 21:47:30 +0900

    fix : 버튼 이벤트 변경

--- src/pages/ReserveProgramPage/ReserveProgramPage.tsx
+++ src/pages/ReserveProgramPage/ReserveProgramPage.tsx
@@ -1,7 +1,7 @@
 //작가의 예약 프로그램 목록을 보여주는 페이지 입니다.
 //Mypage -> 예약 프로그램 (이름/가격 써진 그것) 누르면 나오는 페이지
 import React, {useState} from 'react';
-import {SafeAreaView, Text, ScrollView, StyleSheet, Alert} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
 import * as S from './Style';
 import ReserveProgram from '../../components/Program/ReserveProgram';
 import ContentsHeader from '../../components/ContentsHeader/ContentsHeader';
@@ -35,7 +35,7 @@ const ReserveProgramPage = ({route}) => {
         <S.Container>
           <ReserveProgram title={title} content={content} price={price} />
           {isAuthor ? (
-            <LoginBtn text={'삭제하기'} onPress={handleDeleteProgram} />
+            <View />
           ) : (
             <LoginBtn text={'예약하기'} onPress={handleReserve} />
           )}
